a9083cb060ea1ab1105f85866859c34b1c9926f2
smandke5 <smandke5@gmail.com>
2023-10-14 13:32:23 +0530

    User Id added
    user name added
    
    first name and last name added

--- ConsoleApp_new/User.cs
+++ ConsoleApp_new/User.cs
@@ -6,10 +6,19 @@ namespace ConsoleApp_new
 {
     class User
     {
+        private string userId { get; set; }
+
+        private string username { get; set; }
+
+        private string firstname { get; set; }
+
+        private string lastname { get; set; }
+
         public User()
-        { 
-        
+        {
+
             // constructor added ..
+
         }
 
     }
